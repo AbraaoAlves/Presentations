b6a244d4730a18d96835d445120d097b357a21be
kevin <kevindiegodasilvasousa@gmail.com>
2023-04-17 22:19:49 -0300

    wip: Working github component

--- mdx_apresentation/components/GitHub.tsx
+++ mdx_apresentation/components/GitHub.tsx
@@ -1,24 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import './GitHub.css';
+
+interface GitHubData {
+  name: string;
+  owner: {
+    avatar_url: string;
+  };
+}
+
+const style = {
+  gitHubKevin: 'content',
+  socialMedia: 'socialMedia',
+}
 
 export function GitHub() {
-    function getData() {
-        fetch('https://api.github.com/repos/KevinDik/kevindik')
-            .then(resp => resp.json())
-            .then(data => {
-                const kevinObj = {
-                    nome: data.name,
-                    avatar: data.owner.avatar_url,
-                    primeiro_Login: data.created_at,
-                    link: data.html_url,
-                }
-                return kevinObj;
-            })
-    }
+  const [kevinData, setKevinData] = useState<GitHubData | null>(null);
 
+  useEffect(() => {
+    fetch('https://api.github.com/repos/KevinDik/kevindik')
+      .then((resp) => resp.json())
+      .then((data) => {
+        // Faz nova requisição para obter as linguagens usadas no repositório
+        fetch(data.languages_url)
+          .then((resp) => resp.json())
+          .then((langData) => {
+            const kevinObj: GitHubData = {
+              name: data.name,
+              owner: {
+                avatar_url: data.owner.avatar_url,
+              },
+            };
+            setKevinData(kevinObj);
+          });
+      });
+  }, []);
 
-    return (
+  return (
+    <div className={style.gitHubKevin}>
+      {kevinData && (
         <div>
-            <p>ok</p>
+          <img src={kevinData.owner.avatar_url} alt="Avatar" />
+          <h1>{kevinData.name}</h1>
+          <div className={style.socialMedia}>
+              <ul>
+                <li>GitHub</li>
+                <li>Linkedin</li>
+                <li>Instagram</li>
+              </ul>
+          </div>
         </div>
-    )
+      )}
+    </div>
+  );
 }
